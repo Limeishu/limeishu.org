7236367d8d21753fa00322e08a694cc41c49417b
Aries Cs <aries0d0f@gmail.com>
2017-12-10 00:33:00 +0800

    Add news model.

---
+++ server/model/news.js
@@ -0,0 +1,107 @@
+const express = require('express')
+
+const News = require('../db').News
+const route = express.Router()
+
+route.route('/')
+  .get((req, res) => {
+    News.find({}, (err, doc) => {
+      switch (true) {
+        case err || !doc:
+          res.json({
+            result: -1,
+            err
+          })
+          break
+        case doc:
+          res.json({
+            result: 0,
+            data: doc
+          })
+          break
+      }
+    })
+  })
+  .post((req, res) => {
+    let newNews = new News({
+      title: req.body.title,
+      content: req.body.content,
+      date: new Date(),
+      meta: req.body.meta
+    })
+    newNews.save((err, doc) => {
+      switch (true) {
+        case err:
+          res.json({
+            result: -1,
+            err
+          })
+          break
+        default:
+          res.json({
+            result: 0
+          })
+      }
+    })
+  })
+
+route.route('/:pid')
+  .get((req, res) => {
+    News.findOne({ _id: req.parmas.id }, (err, doc) => {
+      switch (true) {
+        case err || !doc:
+          res.json({
+            result: -1,
+            err
+          })
+          break
+        case doc:
+          res.json({
+            result: 0,
+            doc
+          })
+          break
+      }
+    })
+  })
+  .put((req, res) => {
+    News.findOne({
+      _id: req.parmas.pid
+    }, (err, doc) => {
+      switch (true) {
+        case err:
+          res.json({
+            result: -1,
+            err
+          })
+          break
+        case !doc:
+          res.json({
+            result: -2
+          })
+          break
+        default:
+          News.update({
+            _id: req.parmas.pid
+          }, {
+            title: req.body.title ? req.body.title : doc.title,
+            content: req.body.content ? req.body.content : doc.content,
+            date: new Date(),
+            meta: req.body.meta ? req.body.meta : doc.meta
+          }, err => {
+            switch (true) {
+              case err:
+                res.json({
+                  result: -1,
+                  err
+                })
+                break
+              default:
+                res.json({
+                  result: 0
+                })
+            }
+          })
+      }
+    })
+  })
