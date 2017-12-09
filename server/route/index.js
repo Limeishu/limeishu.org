f34102ad1ee544da4697039c4c65846d4319d56b
Aries Cs <aries0d0f@gmail.com>
2017-12-10 00:38:30 +0800

    Correction static directory path.

--- server/route/index.js
+++ server/route/index.js
@@ -3,6 +3,6 @@ const path                  = require('path')
 
 const route                 = express.Router()
 
-route.use(express.static(path.join(__dirname, '/../dist')))
+route.use(express.static(path.join(__dirname, '/../../dist')))
 
 module.exports = route
