8ee86b3ba17583d3f72eee65fb2df2a33fab92da
Aries Cs <aries0d0f@gmail.com>
2017-12-14 00:45:31 +0800

    Add `Navbar` component and its stylesheets.

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="nav">
+  <div id="nav" class="menu">
     <ul>
       <li v-for="(item, i) in menu">
         <router-link :to="item.path" v-if="item.path">
-          <span class="uppercase soli">{{ item.meta.label[0] || item.name }}</span>
+          <span class="uppercase">{{ item.meta.label[0] || item.name }}</span>
         </router-link>
         <ul v-if="item.children">
           <li v-for="item in item.children">
             <router-link :to="item.path" v-if="item.path">
-              <span class="uppercase soli">{{ item.meta.label[0] || item.name }}</span>
+              <span class="uppercase">{{ item.meta.label[0] || item.name }}</span>
             </router-link>
           </li>
         </ul>
