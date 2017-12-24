dce9ffb367eefa2f6d600981963bd714a5bb08ea
Aries Cs <aries0d0f@gmail.com>
2017-12-24 15:41:37 +0800

    Adjust layout.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
     <header id="header">
       <div class="image" :style="{ 'background-image': `url(${bannerBG[bannerAt - 1]})` }"></div>
       <div id="logo">
-        <img src="./assets/images/icons/logo.svg" alt="">
+        <img src="./assets/images/icons/logo.svg" alt="李梅樹紀念館 Logo">
+        <h1 class="ming">用藝術，說土地的故事</h1>
       </div>
-      <Navbar></Navbar>
+      <Navbar :class="{ 'toggle': toggle }"></Navbar>
     </header>
     <router-view></router-view>
     <mainFooter></mainFooter>
@@ -22,7 +23,8 @@
     data () {
       return {
         bannerBG: [BG.BG1, BG.BG2, BG.BG3, BG.BG4],
-        bannerAt: 1
+        bannerAt: 1,
+        toggle: false
       }
     },
     components: {
@@ -31,6 +33,7 @@
     },
     mounted () {
       this.changeBanner()
+      window.addEventListener('scroll', this.scroll)
     },
     methods: {
       changeBanner () {
@@ -38,6 +41,14 @@
           this.bannerAt = this.bannerAt++ % 4
           this.bannerAt++
         }, 10000)
+      },
+      scroll () {
+        let scrolled = document.querySelector('#app').getBoundingClientRect().y
+        if (scrolled < -(document.querySelector('#header').clientHeight - 64)) {
+          this.toggle = true
+        } else {
+          this.toggle = false
+        }
       }
     }
   }
