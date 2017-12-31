edd65816d0631626b0d84fe6e6f28b6083ec3554
Aries Cs <aries0d0f@gmail.com>
2017-12-31 13:33:55 +0800

    Remove old API server and use `koa` as http server.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,7 @@
-const express           = require('express')
-const bodyParser        = require('body-parser')
-const cors              = require('cors')
+const app         = new (require('koa'))()
+const path        = require('path')
+const config      = require('./config.json')
 
-const app               = express()
-const config            = require('./config.json')
-const route             = require('./route')
-const port              = config.port
-const corsOptions       = {
-  'origin': '*',
-  'methods': 'GET'
-}
-
-app.use(bodyParser.json())
-app.use(route)
-app.use(cors(corsOptions))
-
-app.listen(port)
-console.log(`Start server on port ${port}.`)
+app.use(require('koa-static')(path.join(__dirname, '/../dist/')))
+app.listen(config.port)
+console.log(`Start server on port ${config.port}.`)
--- server/model/index.js
+++
@@ -1,5 +0,0 @@
-const news           = require('./news')
-const post           = require('./post')
-const user           = require('./user')
-
-module.exports = { news, post, user }
