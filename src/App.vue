ed212bba84cde1d998ab73fee5b563789550c3c7
Aries Cs <aries0d0f@gmail.com>
2018-01-17 14:39:37 +0800

    Optimum banner background animate load and fix slogen translate engine error.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header id="header">
-      <div v-if="!isChild" class="image" :style="{ 'background-image': `url(${bannerBG[bannerAt - 1]})` }">
+      <div class="image" v-for="(img, i) in bannerBG" v-show="i === bannerAt % 4" :style="{ 'background-image': `url(${img})` }">
         <div class="next" v-scroll-to="'#home, 0'"><img src="./assets/images/down-arrow.svg" alt=""></div>
       </div>
       <div v-if="!isChild" id="logo">
@@ -26,13 +26,13 @@
     data () {
       return {
         bannerBG: [BG.BG1, BG.BG2, BG.BG3, BG.BG4],
-        bannerAt: 1,
+        bannerAt: 0,
         toggle: false,
         isChild: false,
         slogen: {
-          zh_TW: '用藝術，說土地的故事',
-          en_US: 'Telling the stories of land through art',
-          ja: '芸術からこの地の物語をつむぐ'
+          'zh-TW': '用藝術，說土地的故事',
+          'en-US': 'Telling the stories of land through art',
+          'ja': '芸術からこの地の物語をつむぐ'
         }
       }
     },
@@ -50,7 +50,6 @@
     methods: {
       changeBanner () {
         setInterval(() => {
-          this.bannerAt = this.bannerAt++ % 4
           this.bannerAt++
         }, 10000)
       },
