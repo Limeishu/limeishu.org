2ee399fd466a00219c992e799a46252b594e1329
Aries Cs <aries@Ariess-MacBook-Pro.local>
2018-01-26 18:56:40 +0800

    Fix restric uri.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const app         = new (require('koa'))()
 const path        = require('path')
+const hAF         = require('koa2-connect-history-api-fallback')
 const config      = require('./config.json')
 
+app.use(hAF())
 app.use(require('koa-static')(path.join(__dirname, '/../dist/')))
 app.listen(config.port)
 console.log(`Start server on port ${config.port}.`)
