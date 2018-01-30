3d798d35250065a8ffb96ba6e5da96647d8c223d
Aries Cs <aries0d0f@gmail.com>
2018-01-30 18:23:33 +0800

    Add news reader component.

---
+++ src/components/layout/Reader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="spotlight inner">
+    <div class="reader" v-html="news.paragraph">
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex'
+  export default {
+    data () {
+      return {
+        news: {}
+      }
+    },
+    mounted () {
+      this.getNews()
+    },
+    methods: {
+      ...mapActions(['getNewsByID']),
+      async getNews () {
+        try {
+          const res = await this.getNewsByID(this.$route.params.id)
+          this.news = res.data
+        } catch (err) {
+          console.log(new Error(err))
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,9 @@
 import mainFooter       from './mainFooter.vue'
 import Navbar           from './Navbar.vue'
+import Reader           from './Reader.vue'
 
 export {
   mainFooter,
-  Navbar
+  Navbar,
+  Reader
 }
