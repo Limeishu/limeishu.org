6bb8fb9820bb4c1a49e2b24cfaeea046c7bf0389
Aries Cs <aries0d0f@gmail.com>
2018-01-30 17:52:52 +0800

    Add news card.

--- src/components/Culture/News.vue
+++ src/components/Culture/News.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-
+  <div class="card-container">
+    <div class="card" v-for="(news, i) in news.data" :key="i">
+      <div class="image" :style="{ 'background-image': `url(${news.meta.image})` }">
+        <h2>{{ news.title }}</h2>
+      </div>
+      <div class="content">
+        <span class="date">{{ new Date(news.date).toLocaleDateString() }}</span>
+        <p>{{ `${news.content.substring(0, 50)}...` }}</p>
+        <router-link :to="`/culture/news/${news._id}`" class="button">
+          <span>閱讀全文</span>
+          <font-awesome-icon icon="angle-right" />
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  // export default {
-  //
-  // }
+  import { mapGetters, mapActions } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['news'])
+    },
+    mounted () {
+      if (!this.news.data) {
+        this.getAllNews()
+      }
+    },
+    methods: {
+      ...mapActions(['getAllNews', 'getNewsByID'])
+    }
+  }
 </script>
