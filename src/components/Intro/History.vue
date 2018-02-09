098c6f2efe8ae3a44560102bd0fa92f33d72d94f
Aries Cs <aries0d0f@gmail.com>
2018-02-09 13:59:55 +0800

    Add history components.

--- src/components/Intro/History.vue
+++ src/components/Intro/History.vue
@@ -1,11 +1,58 @@
 <template>
   <div>
-
+    <div class="paragraph">
+      <h1>{{ $route.meta.label[language] }}</h1>
+    </div>
+    <div class="timeline-container">
+      <div class="timeline" v-for="(doc, i) in docs[language]" :key="i">
+        <div class="date"><span>{{ doc.date }}</span></div>
+        <div class="content">
+          <div v-for="(item, j) in doc.content" :key="j">
+            <h3 v-if="doc.content[j].length > 0">{{ tagTranslated[j][language] }}</h3>
+            <p v-for="(item, k) in item" :key="k">{{ item }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  // export default {
-  //
-  // }
+  import { mapGetters, mapActions } from 'vuex'
+  export default {
+    data () {
+      return {
+        docs: {},
+        tagTranslated: {
+          centry: {
+            'zh-TW': '百年紀要',
+            'en-US': 'Centennial Chronicles',
+            'ja': '百年紀要'
+          },
+          art: {
+            'zh-TW': '國內外藝壇',
+            'en-US': '',
+            'ja': '国内外美術関係事項'
+          },
+          event: {
+            'zh-TW': '國內外社會重要記事',
+            'en-US': '',
+            'ja': '国内外社会重要事項'
+          }
+        }
+      }
+    },
+    computed: {
+      ...mapGetters(['language'])
+    },
+    mounted () {
+      this.getDocs()
+    },
+    methods: {
+      ...mapActions(['getDocsByType']),
+      async getDocs () {
+        this.docs = await this.getDocsByType('limeishu-chronicles')
+      }
+    }
+  }
 </script>
