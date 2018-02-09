c62b514d3828185d7f67dba9ec9cf50edc43d131
Aries Cs <aries0d0f@gmail.com>
2018-02-09 10:58:03 +0800

    Add Article contents.

--- src/components/Intro/Article.vue
+++ src/components/Intro/Article.vue
@@ -1,11 +1,53 @@
 <template>
   <div>
-
+    <div class="paragraph" v-show="language === 'zh-TW'">
+      <h1>畫家介紹</h1>
+      <p>李梅樹（一九○二～一九八三）早年就讀總督府國語學校師範部，深受西洋繪畫的薰染，習畫的意念油然興起。早期畫作一九二七《靜物》；一九二八《三峽後街》先後入選第一、二屆臺展。一九二八年赴東京，奔波習畫於川端、同舟社及本鄉三個繪畫研究所。翌年，通過東京美術學校（東京藝大前身）入學試，師事畫壇大師長原孝太郎、小林萬吾及岡田三郎助等，奠定其後寫實風格的深厚基礎。一九三四年與友陳澄波、顏水龍、楊三郎、廖繼春、陳清汾、李石樵、立石鐵臣共創《臺陽美術協會》。一九三五年《小憩之女》獲第九屆臺展特選第一席，受頒「臺灣總督獎」。一九三九年，《紅衣》入選第三屆文展（原日本帝展）。一九四○年《花與女》入選第四屆文展（奉祝展），自此奠定其為臺灣傑出之西洋美術家的歷史地位。一九四六年應聘首屆省展審查委員，參展之《星期日》由省府購呈蔣總統。一九四八年《黃昏》、《郊遊》等大幅畫作被視為具代表性之作品。一九四五年臺灣光復後，除涉足三峽地方政治，並於一九四七年受公推主持長福巖清水祖師廟重建工作。此後畫作題材、風格日益趨向鄉土寫實，表現其特立獨行，傲視群倫的大家風範。一九六二年以後並歷任文化大學、國立藝專、師大等美術教授、主任等教職。指導藝專等雕塑科學生參與三峽祖師廟金屬浮雕工程，將學院專業技藝投注於傳統寺廟建築實務中，乃臺灣藝術教育史之創舉。李教授對美術教育推廣的貢獻，除學院正規教育外，並以嚴謹、精湛的美學素養，指導祖師廟聘請的諸多傳統木石雕刻師，造就祖師廟成為雕刻精品推砌而成的「東方藝術殿堂」，並為臺灣現代寺廟建築的經典名作。晚年受美術界推選歷任油畫學會理事長、美協理事長、油畫學會榮譽理事長等崇高之職位。</p>
+      <p>李梅樹紀念館執行長 李景文</p>
+    </div>
+    <div class="paragraph" v-show="language === 'ja'">
+      <h1>画家の紹介</h1>
+      <p>李梅樹（1902～1983）は、早年に総督府国語学校師範部に学び、そこで西洋絵画の薫陶を受けて、絵画を学びたいという願いが湧き起こりました。初期の作品である1927年の《静物》、1928年の《三峡の町裏》は、第一回、第二回台湾美術展覧会にそれぞれ入選。1928年には東京に渡り、川端美術学校、同舟社、本郷美術学校の三ヵ所の絵画研究所で絵を学びました。翌年、東京美術学校（東京芸大の前身）の入学試験に合格し、長原孝太郎、小林萬吾、岡田三郎助ら当時の画壇の重鎮に師事、その後に続く写実的スタイルの基礎を固めました。1934年に友人の陳澄波、顔水龍、楊三郎、廖継春、陳清汾、李石樵、立石鉄臣らとともに「台陽美術協会」を創設。1935年に《憩ふ女》が第九回台展の特選第一等となり、「台湾総督賞」を受賞します。1939年には、《赤い衣》が日本で第三回新文展に入選、1940年に《花と女》が第三回新文展（奉祝展）に入選するなど、以後台湾の優れた西洋美術家としての歴史地位を確かなものとしました。1946年、第一回台湾省美術展覧会の審査委員となり、出品した《星期日（日曜日）》は台湾省政府から蒋介石総統に贈呈されました。1948年の《黄昏》、《郊遊（ピクニック）》といった大作は、画家の代表作とされています。1945年の台湾光復後、三峡の地方政治に足を踏み入れたほか、1947年には長福巌清水祖師廟再建工事の責任者に推挙されました。その後から、作品の題材やスタイルはしだいに郷土の写実へと向かい、その表現は独自の道を歩み、傑出した大家としての風格を帯びて行きます。1962年以後は、文化大学、国立芸術専科学校、師範大学などの美術教授や主任などの教職を歴任。芸専等の雕塑科学生を指導して三峡祖師廟の金属浮雕工事に参加させ、アカデミックな専門技術を伝統寺廟建築の現場に投入したことは、台湾芸術教育史において初めての試みでした。李梅樹による美術教育推進への貢献は、大学での正規教育以外にも、厳粛で精緻な美的素養でもって、祖師廟に招いた多くの伝統的木彫・石彫師らを指導し、祖師廟は彫刻の名品がつまった「東方芸術の殿堂」へと作り上げられ、現代台湾寺廟建築の古典的名作となりました。晩年には、美術界からの推薦により、油画学会理事長、美協理事長、油画学会栄誉理事長等の職を歴任しました。</p>
+    </div>
+    <div class="paragraph" v-show="language === 'en-US'">
+      <h1>Introduction to Artist</h1>
+      <p>Li Mei-Shu (1902～1983) studied at Teacher Training Department Taiwan Governor-General’s National Language in the
+          early days and grew interest in painting under the profound influence from western painting. His early painting,
+          1927 “Still Life” and 1928 “Street of Sanxia” entered the first and second Taiwan Exhibition. In 1928 he went to
+          Tokyo and studied painting with three painting graduate institutes, namely Kawabata, Tongzhou-She (同舟社) and Hongo. In the following
+          year, he passed the admission examination of Tokyo Art School (former Tokyo University of the Arts) and studied
+          from maters Koutarou Hara, Kobayashi Mango and Saburosuke Okada from the painting industry, solidifying his profound
+          foundation for realism style later. In 1934, Li Mei-Shu co-established the “Taoyang Arts Association” with his
+          friends Chen Cheng-Po, Yen Shui-Long, Yang San-Lang, Liao Ji-Chun, Chen Qing-Pan, Li Shi-Chiao, and Tetsuomi Tateishi.
+          In 1935, “The Lady in Rest” won the first place in the 9th Taiwan Exhibition and was presented with “Taiwan Governor
+          General’s Award.” In 1939, the “Girl in The Red Dress” was selected in the Third Japanese Ministry of Education‘s
+          Art Exhibition (previously known as Japanese Emperor Exhibition). His “Girl with Flowers” was selected in the Fourth
+          Culture Exhibition (State founding of Japan) in 1940, where his position as outstanding Taiwan western artist was
+          defined in history since then. In 1946, Li was appointed as the first Provincial Exhibition Review Committee member
+          and his work “Sunday” participating in the exhibition was purchased by the Provincial Governor and given to President
+          Chiang at the time. In 1948, large-scale paintings such as “Dusk” and “Field Trip” were regarded as the representative
+          works of Li Mei-Shu. By 1945 and after Taiwan Retrocession, Li not only involved in the local politics of Sanxia
+          but was also recommended to host the reconstruction work of Changfuyen Qingshui Zushi Temple. From then on, the
+          topics and styles of his painting approached rustic realism and exhibited his unique style and exceptionality.
+          In 1962, he also served as the art professor and director for Chinese Culture University, National Art College
+          and National Normal University. He advised students of sculpture department from National Art College to participate
+          in the metal relief project of Sanxia Zushi Temple, projecting the professional techniques of academic school to
+          the building practice of traditional temple, which was the first in the art education history of Taiwan. Professor
+          Li’s contribution in promoting art education not only comprised standard education of the academy but also applied
+          rigorous and splendid aesthetic quality in supervising many traditional wood and stone sculpture masters employed
+          by Zushi Temple. These masters have built Zushi Temple into an “Oriental Art Palace” consisting carving boutiques
+          and Zushi Temple became the classic masterpiece of modern temple building in Taiwan. In his late years, he was
+          recommended by the Art Industry and selected as the Oil Painting Association Director, Art Association Director,
+          Oil Painting Association Honorary Director, and may esteemed positions.</p>
+    </div>
   </div>
 </template>
 
 <script>
-  // export default {
-  //
-  // }
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['language'])
+    }
+  }
 </script>
