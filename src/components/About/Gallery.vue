203d0fc3b2951f021976d5ec45dde1b9b190a2e0
Aries Cs <aries0d0f@gmail.com>
2018-02-09 10:23:16 +0800

    Add gallery content.

--- src/components/About/Gallery.vue
+++ src/components/About/Gallery.vue
@@ -1,11 +1,106 @@
 <template>
   <div>
-
+    <div class="paragraph" v-show="language === 'zh-TW'">
+      <h1>關於紀念館</h1>
+      <h2>簡介</h2>
+      <p>本館初設於1990年，原名為「劉清港醫師李梅樹教授昆仲紀念館」；1995年4月遷入現址：台北市三峽區中華路43巷10號，並正式定名為「李梅樹紀念館」。</p>
+      <p>紀念館設立之宗旨在紀念前輩畫家李梅樹先生。於三峽（三角湧）土生土長的李梅樹先生（1902-1983），生前致力於藝術創作，美術運動，一輩子堅持以東京美術學校習得的寫實路線，擁抱台灣鄉土之美，被稱為台灣美術運動中的「萬里長城」。</p>
+      <p>1947年開始，李梅樹先生將畢生的精力投入三峽祖師廟的重建工程上，以創作藝術品的精神，完成結合民間藝術與學院藝術的「東方藝術殿堂」。</p>
+      <p>此外，熱心公益的李梅樹先生，於1935至1958年間，陸續以三峽街庄協議員，縣議員等身分服務鄉里。日後並於藝專(現國立台灣藝術學院)、文化、師大等校任教。終其一生，李氏身兼藝術家，教育家，政治家與廟宇建築家，八十一年多采多姿的藝術生涯，在台灣美術史上寫下燦爛傳奇的一頁。</p>
+      <p>本館除了展出李梅樹先生不同時期的畫作，另一方面，李梅樹生前的書信文件，畫具和畫稿等，也於館內的文物陳列室一一展出。</p>
+      <p>期盼透過本館的展覽，不僅使參觀者得以體會藝術家創作生涯的心路歷程，更能進一步明瞭，李梅樹的一生正是台灣美術走向本土化的縮影。</p>
+      <h2>參觀需知</h2>
+      <ul>
+        <li>本館採收費參觀制，清潔費每位100元、敬老/學生/軍警每位80元、嬰幼兒免費。</li>
+        <li>開放參觀時間:每週六、日，10:00AM ~ 05:00PM。其餘時段，歡迎團體預約參觀。(團體預約參觀時間，可提早自上午九點開始，詳情請洽詢本館)</li>
+        <li>可安排團體導覽三峽祖師廟、李梅樹紀念館、三峽老街及海山學文物展覽館，共需時約三小時。(若要參觀海山學文物展覽館，需提前兩週預約)</li>
+        <li>額外行程視情況酌收額外導覽費。</li>
+        <li>入館參觀請勿攝影，請勿飲食，請勿吸煙。</li>
+        <li>未滿12歲兒童請由家長陪伴參觀。</li>
+        <p>其他參觀細則請洽
+          <a href="tel:0226732333">(02)2673-2333</a> 或 E-mail :
+          <a href="malito:limeishu@gmail.com">limeishu@gmail.com</a>
+        </p>
+      </ul>
+      <h2>李梅樹文教基金會</h2>
+      <p>1990年成立至今的「三峽李梅樹紀念館」是台灣頗具代表性的個人美術館，並已正式指定成為文建會屬下的「地方文化館」，為台北縣增加一處休閒、教育及觀光的景點。</p>
+      <p>李梅樹將畢生的精力投入三峽祖師廟的重建工程，並熱心公益，曾任縣議員及農會理事長等身分服務鄉里，日後並於國立藝專（現國立台灣藝術大學）、文化、師大等校任教，終其一生，李梅樹身兼藝術家、教育家、政治家與廟宇建築家，以八十一年多采多姿的藝術生涯，在台灣美術史上寫下燦爛傳奇的一頁。家族基於紀念先人的精神及遺留甚多的作品文物，自籌資金自民國七十九年於三峽鎮設立李梅樹紀念館，隨後亦成立李梅樹文教基金會，至今已屆滿十五週年。</p>
+      <p>館長李景光先生表示，一路走來「空間」、「經費」與「傳承」是李梅樹紀念館遇到的關關難題，目前的館址是貸款買來的房子，光是花在硬體設施就砸了六千萬，但因為李梅樹生前就很少賣畫、很少展覽，想要自己的作品好好留在身邊，因此儘管再困難，他們三兄弟五姊妹都希望夠永續經營李梅樹紀念館。</p>
+      <p>現在已成立的李梅樹文教基金會，就是希望未來將李梅樹的畫作都交由基金會管理，成為其資產。此外，目前也在三峽民生街籌備李梅樹文物紀念館，以展覽家族收藏的骨董文物。李景光說，成立私人美術館必須把它當作一種運動，將其看做是台灣文化的重要事情，而非賺錢工具。只是經費之外，他更擔心的是「傳承」的問題，他這一代願意為紀念館奉獻，但下一代的意向卻是無法掌握與強求的，因此，李景光奉勸想要成立私人紀念館典藏作品的朋友真的要三思而後行，做了就勇往直前不後悔。</p>
+      <p>由於紀念館年久失修，早期又沒有設立無障礙設施，幸文建會專款補助及台北縣政府文化局協助經後，過將近兩年的籌劃及改善軟硬體的設施，已符合政府規定，榮獲正式指定成為文建會屬下的「地方文化館」。文化局長林泊佑表示，李梅樹紀念館致力於推廣美術教育及三峽祖師廟傳統藝術之美學，結合社區及學校發揮終身學習的功能，鼓勵全民體驗美術與人文、歷史、風俗習慣及週遭環境的變化。並以畫中實景、人與物等關係為教材提供大眾及學生們對於美學有比較清楚的認識，今天成為地方文化館，可謂實至名歸。</p>
+      <p>李梅樹文教基金會致力於其李梅樹先生的作品妥善管理與保存， 九十三年底由國家圖書館委託傳統工藝學系，完成登錄與整理李梅樹文教基金會所藏畫家「李梅樹手繪三峽祖師廟設計圖稿」，目的為重現祖師廟最原始藝術構想，並應證藝術家在創作與生活空間，相互交融的真切。</p>
+      <p>將近四百件的手稿，由六位傳統工藝系同學負責登錄整理工作。於文教基金會點交數量後，予以逐張攤開拍照數位存檔，並依據紙質、內容等條件，區分工程圖及圖稿二類，最後詳實紀錄圖稿上任何污漬缺損，以便日後托底、裱褙處理及數位化工作的進行。</p>
+      <p>手繪圖稿在李梅樹文教基金會與國家圖書館妥善保存下，泛黃的紙張、隱約的筆跡，得以重現畢生致力關懷鄉土與美術運動的藝術家李梅樹，生命中最熱切的溫度。</p>
+      <p>私人美術館走的辛苦，但是，現實是殘酷的，在政府對藝術品的保存及典藏制度未能有明確法案依循，美術館管理事權統一的法案也仍在排隊，文建會現在的管轄範圍只有國立台灣美術館，加上補助私人美術館政府經費實在有限，期許各界愛好藝術的人士，歡迎捐助李梅樹文教基金會。</p>
+      <a href="https://nas.limeishu.org.tw/share/%E5%9F%BA%E9%87%91%E6%9C%83%E7%AB%A0%E7%A8%8B.pdf" class="button" target="_blank"><span>基金會章程</span><font-awesome-icon icon="angle-right" /></a>
+    </div>
+    <div class="paragraph" v-show="language === 'ja'">
+      <h1>記念館について</h1>
+      <h2>紹介</h2>
+      <p>記念館の設立趣旨は、台湾第一世代の近代画家・李梅樹を記念するものです。三峡（三角湧）の地に生まれ育った李梅樹（1902-1983）は、生前芸術制作や美術運動に力を注ぎ、東京美術学校で習得した写実的な画風を生涯に渡って貫きながら、台湾郷土の美を愛し、台湾の美術運動における「万里の長城」と称されました。</p>
+      <p>1947年より、李梅樹は三峡の祖師廟再建工事に生涯の精力を傾け、芸術作品を創作しようという意識の下、民間芸術とアガデミックな美術を結合させた「東方芸術の殿堂」を完成させました。</p>
+      <p>また、公共活動にも熱心であった李梅樹は、1935年から1958年の間、三峡街庄協議員、県議員等の役職に就いて郷里のために尽くしました。その後、国立台湾芸術専科学校(現在の国立台湾芸術大学)、文化大学、師範大学でも教壇に立ちました。その一生に渡って、李梅樹は芸術家・教育家・政治家・寺廟建築家といった身分を兼ね備え、81年間の多彩で多様なその芸術の生涯は、台湾美術史上において輝かしい一ページを残しました。</p>
+      <p>本館では李梅樹の様々な時期の作品を展示しているほか、李梅樹の生前の書簡、絵画道具、画稿などを館内の文物陳列室にともに展示しています。</p>
+      <p>本館の展示を通じ、ご来館の皆様にこの芸術家の画家人生における心の旅路を感じて頂くとともに、李梅樹の一生が台湾美術の本土化への縮図であったことを理解して頂くことを願っております。</p>
+      <h2>参観案内</h2>
+      <ul>
+        <li>本館は有料参観制で、清掃費として以下の料金を頂いております。一般 NTD 100元、敬老／学生／軍警 NTD 80元、乳幼児無料。</li>
+        <li>開館時間:毎週土・日祝、午前10:00-午後5:00。その他の時間については、団体予約による参観をお待ちしています。(団体予約の参観時間は午前9時開始も可能です。詳細は本館にお問合せ下さい。)</li>
+        <li>三峡祖師廟、李梅樹記念館、三峡老街、海山学文物展覧館の団体ガイドツアーを組むことも可能です。総時間は約三時間です。(海山学文物展覧館の参観をご希望の場合は、二週間前までにご予約下さい。)</li>
+        <li>これ以外のコースについては、状況により追加ガイド費を徴収する場合があります。</li>
+        <li>入館時における、写真撮影・飲食・喫煙はご遠慮下さい。</li>
+        <li>12歳未満の児童の参観には、保護者の方の付き添いが必要です。</li>
+        <p>その他、参観に関することについては、こちらまでお問合せ下さい。<br>電話 : <a href="tel:0226732333">(02)2673-2333</a>、E-mail : <a href="malito:limeishu@gmail.com">limeishu@gmail.com</a></p>
+      </ul>
+      <h2>基金会について</h2>
+      <p>1990年に成立し今日まで続く「三峡李梅樹記念館」は、台湾を代表する個人美術館であり、行政院文化建設委員会に属する「地方文化館」として正式に指定され、新しい台北のレジャー・教育・観光の拠点となっています。</p>
+      <p>李梅樹は三峡の祖師廟再建工事に生涯の精力を傾けるとともに、公共活動にも熱心に取り組み、県議会議員・農会理事長といった役職に就いて郷里のために尽くしました。その後、国立台湾芸術専科学校(現在の国立台湾芸術大学)、文化大学、師範大学でも教壇に立ちました。その一生に渡って、李梅樹は芸術家・教育家・政治家・寺廟建築家の身分を兼ね備え、81年間の多彩で多様なその芸術の生涯は、台湾美術史上において輝かしい一ページを残しました。家族はこの先人の精神と残された多くの作品や遺品を顕彰するため、自費によって1990年三峡に李梅樹記念館を設立、その後李梅樹文教基金会を作って、今日に至っています。</p>
+      <p>館長の李景光は、「空間」・「経費」・「継承」の問題がこれまで李梅樹記念館が直面して来た難題であったとしています。李梅樹は生前絵を売ったり、展示をしたりすることが少なく、自身の作品を身の回りに留めて置くことを望んでいました。そのため困難ではあるものの、私ども三兄弟五姉妹は、李梅樹記念館が今後も永続的に経営できることを願ってやみません。</p>
+      <p>現在、三峡民生街に設置準備をしている李梅樹文物記念館では、家族が収蔵する骨董や文物をも展示し、台湾文化の伝承に尽力していく次第です。</p>
+      <p>李梅樹記念館は、美術教育や三峡祖師廟の伝統芸術の美を広めるため、地域や学校と協力して生涯学習に貢献し、人々が美術、人文、歴史、風習、周辺環境の変化といったものへの体験を推進しています。また、絵画作品の中の実景、人や物の関係を教材として、一般の人々や学生らに美術に対してより確かな認識を持ってもらう試みを行っています。今日、本館は地方文化館として確かな成果を挙げていると言えるでしょう。</p>
+      <p>手描き画稿は、李梅樹文教基金会と国家図書館の適切な保存により、黄ばんだ紙、薄れた筆跡から、郷土を想い美術運動に生涯尽力した芸術家・李梅樹のその生命の最も熱い情熱を汲み取ることが出来ます。</p>
+      <p>各界の美術愛好者の皆様からの李梅樹文教基金会へのご支援・ご寄付をお待ちしております。</p>
+      <a href="https://nas.limeishu.org.tw/share/%E5%9F%BA%E9%87%91%E6%9C%83%E7%AB%A0%E7%A8%8B.pdf" class="button"><span>基金会規約</span><font-awesome-icon icon="angle-right" /></a>
+    </div>
+    <div class="paragraph" v-show="language === 'en-US'">
+      <h1>About the Memorial Gallery</h1>
+      <h2>Introduction</h2>
+      <p>The gallery was established in 1990  and previously named as “Liu Qing Gang Physician and Professor Li Mei-Shu Memorial Hall.” In April 1995, the galley was relocated to the current location: No. 10, LN. 43, Zhonghua Road., Sanxia Dist., New Taipei City and officially renamed to “Li Mei-Shu Memorial Gallery.” </p>
+      <p>The purpose of establishing the memorial gallery was in commemoration of predecessor artist, Professor Li Mei-Shu. Professor Li Mei-Shu was born and raised in Sanxia (Sanjiaoyong) locally (1902-1983) who was committed to art creation before his death, art movement and lifelong persistence in the realism style acquired from Tokyo University of the Arts, embracing the rustic beauty of Taiwan and is known as the “Great Wall” in the art movement of Taiwan. </p>
+      <p>Starting from 1947, Professor Li Mei-Shu committed his lifelong devotion into the reconstruction project of Sanxia Zushi Temple, in order to complete the “oriental art palace” combining civil arts and academic arts through creative art pieces.</p>
+      <p>Moreover, Professor Li Mei-Shu showed much enthusiasm in charity. Between 1935 and 1958, Professor Li served as the reconciliation staff for Sanxia Street and county councilman to provide services to the township. Later he instructed in National Art College (now known as the National Taiwan University of Arts), Chinese Culture University, National Normal University and many other universities. In his lifelong career, Li has been an artist, educator, politician, and temple architect whose 81 years of remarkable art career opened up a brilliant and legendary chapter in the art history of Taiwan.</p>
+      <p>Apart from exhibiting Professor Li Mei –Shu’s paintings during different stage, the letters written, painting tools and sketches before Li’s death, were also displayed at the cultural display room of the gallery.</p>
+      <p>The gallery expects to help visitors experience the course of the creation career as an artist through the exhibition but also further understand the life of Li Mei-Shu was a miniature of the progress of Taiwan’s art towards localization.</p>
+      <h2>Instructions for Visit</h2>
+      <ul>
+        <li>The gallery admission charges each person with a cleaning fee of NT100 and NT80 for elderly/student/military and police. The gallery admission for infants and babies are free.</li>
+        <li>Opening hours for visits are Saturdays and Sundays, from 10:00AM to 05:00PM. In other times, group reservation for visit is encouraged (The visit time for group reservation starts as early at 9A.M. Please consult with the gallery for more information).</li>
+        <li>A group tour on Sanxia Zushi Temple, Li Mei-Shu Memorial Gallery, Sanxia Old Street, and Center for Haishan Research can be arranged for a total of 3 hours. (Visit to Center for Haishan Research requires reservation in two weeks in advance).</li>          <li>Additional itinerary might charge additional guide fees depending on the situation.</li>
+        <li>入Do not film the gallery when admitted for visit. Food, drinks and smoking are prohibited.</li>
+        <li>Children visiting under 12 years old require the company of parents.</li>
+          <p>Please contact
+            <a href="tel:0226732333">(02)2673-2333</a> or E-mail :
+            <a href="malito:limeishu@gmail.com">limeishu@gmail.com</a>for more information on the visit.
+          </p>
+      </ul>
+      <h2>Introduction to Foundation</h2>
+      <p>The “Sanxia Li Mei-Shu Memorial Hall” established since 1990 is a personal art gallery representing Taiwan, which has been officially designated into a “local cultural museum” under the Department of Cultural Affairs, adding a new leisure, education and tourism scenic spots for New Taipei City.</p>
+      <p>Li Mei-Shu has devoted his lifetime of efforts in the reconstruction project of Sanxia Zushi Temple with enthusiasm in charity. He served as county councilman and farmers’ association director to provide services to fellowmen, followed by instructing at the National Art College (now the National Taiwan University of Arts). Chinese Culture University and National Normal University. In his lifelong career, Li has been an artist, educator, politician, and temple architect whose 81 years of remarkable art career opened up a brilliant and legendary chapter in the art history of Taiwan. In commemoration of the predecessor’s spirit and many works and relics, the family of Li raised their own funds since 1990 to establish the Li Mei-Shu Memorial Gallery in Sanxia Township, followed by establishing the Li Mei-Shu Cultural and Educational Foundation even today.</p>
+      <p>Gallery Director Mr. Li Jing-Guang stated that the difficulty encountered by Li Mei-Shu Memorial Gallery included “space,” “funds” and “legacy.” Since Li Mei-Shu seldom sold paintings or exhibited his painting before passing away but he intended to preserve his works with him, the three brothers and five sisters intended to operate sustainable management of Li Mei-Shu Memorial Gallery in spite of the difficulties.</p>
+      <p>Currently the establishment of Li Mei Shu Relics Memorial Museum is being prepared at Minsheng Street, Sanxia, which exhibits the antique relics collected by the family to contribute efforts in the passing of Taiwan’s culture.</p>
+      <p>Li Mei-Shu Cultural and Educational Foundation is committed to the safeguard and preservation of works from Mr. Li Mei-Shu. “Li Mei-Shu’s hand-drawn Sanxia Zushi Temple Design Draft” reproduces the most primitive artistic concept of Zushi Temple with proof on the ingenuity of mutual fusion between the creation and living space by artists.</p>
+      <p>Hand-drawn drafts are safe-guarded by Li Mei-Shu Cultural and Educational Foundation and the National Library, where the yellowed paper and faint traces of the drawing are reproduced to the lifelong commitment of artist Li Mei-Shu in care for local and art movement, demonstrating the most zeal warmth of life.</p>
+      <p>We look forward to art fans of all industries to make contribution to Li Mei-Shu Cultural and Educational Foundation.</p>
+      <a href="https://nas.limeishu.org.tw/share/%E5%9F%BA%E9%87%91%E6%9C%83%E7%AB%A0%E7%A8%8B.pdf" class="button" target="_blank"><span>Articles of Incorporation of the Foundation</span><font-awesome-icon icon="angle-right" /></a>
+    </div>
   </div>
 </template>
 
 <script>
-  // export default {
-  //
-  // }
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['language'])
+    }
+  }
 </script>
