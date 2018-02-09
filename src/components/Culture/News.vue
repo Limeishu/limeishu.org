099d8ab5047409491872147aff373e788109fac8
Aries Cs <aries0d0f@gmail.com>
2018-02-09 14:28:43 +0800

    Fix lost title.

--- src/components/Culture/News.vue
+++ src/components/Culture/News.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="card-container">
-    <div class="card" v-for="(news, i) in news.data" :key="i">
-      <div class="image" :style="{ 'background-image': `url(${news.meta.image})` }">
-        <h2>{{ news.title }}</h2>
-      </div>
-      <div class="content">
-        <span class="date">{{ new Date(news.date).toLocaleDateString() }}</span>
-        <p>{{ `${news.content.substring(0, 50)}...` }}</p>
-        <router-link :to="`/culture/news/${news._id}`" class="button">
-          <span>閱讀全文</span>
-          <font-awesome-icon icon="angle-right" />
-        </router-link>
+  <div class="paragraph">
+    <h1>{{ $route.meta.label[language] }}</h1>
+    <div class="card-container">
+      <div class="card" v-for="(news, i) in news.data" :key="i">
+        <div class="image" :style="{ 'background-image': `url(${news.meta.image})` }">
+          <h2>{{ news.title }}</h2>
+        </div>
+        <div class="content">
+          <span class="date">{{ new Date(news.date).toLocaleDateString() }}</span>
+          <p>{{ `${news.content.substring(0, 50)}...` }}</p>
+          <router-link :to="`/culture/news/${news._id}`" class="button">
+            <span>閱讀全文</span>
+            <font-awesome-icon icon="angle-right" />
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -20,7 +23,7 @@
   import { mapGetters, mapActions } from 'vuex'
   export default {
     computed: {
-      ...mapGetters(['news'])
+      ...mapGetters(['news', 'language'])
     },
     mounted () {
       if (!this.news.data) {
