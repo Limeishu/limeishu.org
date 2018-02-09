9f6d8b9b7508e16ab51b3e95ca84f5e3c2c94367
Aries Cs <aries0d0f@gmail.com>
2018-02-09 10:55:18 +0800

    add content of Personal Statement of the Artist.

--- src/components/Intro/Statement.vue
+++ src/components/Intro/Statement.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-
+    <div class="paragraph" v-show="language === 'zh-TW'">
+      <h1>畫家自述</h1>
+      <h2>一甲子真善美的追尋</h2>
+      <p>「我生長於藝術氣氛濃厚的家庭，因受環境薰陶影響，自幼對美術，音樂即有非常的興趣；尤其對繪畫更為醉心。1918 年考取日據時代台灣總督府國語學校（即今省立台北師專前身），開始接觸較正式之美術教育後，對繪事更加著迷，矢志以繪畫為業，計畫在師範學校畢業後，即赴日入東京美術學校（即今日日本國立東京藝術大學前身）進修；惜因先父不允，不克成行。不得已，於1922年師範學校畢業後，返鄉任教，並奉父命於是年完婚。</p>
+      <p>父親之意，希望藉此打消我東渡日本求學的意念。但是歲月的飛逝，不僅無法平息我追求藝術的熱情，反而使我更加殷切盼望，能夠早日實現衷心嚮往的理想。因此課餘之暇，更加專心致力研究畫藝，並利用暑假返回母校，參加石川欽一郎先生所組之[暑期美術講習會]。工作之餘所繪作品，連續入選第一，二屆台展，家人也因而了解我的稟賦與志向，不再堅持要我往政治方面發展。</p>
+      <p>1924年，先父逝世，其後數年，我的努力成果獲得先兄等人之信心，終於在1928年得償宿願，以二十七歲之年，毅然負笈日本，以期在藝術上更上層樓，舒展抱負。初抵東京，日以繼夜，加強鍛鍊素描，歷經四月，隔年三月，僥倖通過東京美術學校入學考試就讀西洋畫科。由於先兄之鼓勵，所以能夠毫無後顧之憂，全力投注繪事。</p>
+      <p>不料好景不常，先兄竟在我二年級第一學期行將結束之際，遽然因病去世。家中重責頓時落在我的肩上，不得已休學一學期，暫留台灣。由於先兄逝世，家中遭受重大打擊，先母及親友紛紛勸我放棄再赴日本學畫，接受地方優渥公職;終因理想的驅策，使我堅持貫徹初衷，在家務料理妥當後，毅然再束裝赴日，繼續未完成的學業。或許天性使然，所以在日本求學期間，雖然正值野獸主義等繪畫思潮風行日本畫壇，造成極大的衝擊;卻始終不曾動搖自己的理想，仍然秉持一貫的自信與執著，堅決肯定自己所喜好的寫實創作風格。五十年代，國內抽象繪畫風起雲湧，熱鬧非凡，一時附和之聲四起;但這一切都不足以搖憾我的信念，依舊朝著寫實這條筆直而深入的道路昂然前進，無視外界任何批評與褒貶。</p>
+      <p>因為童年一直生活在純樸的鄉野，所以對故鄉有著一份深厚無比的感情。壯年從政多年之後，對於傳統生活文化，得以另一個截然不同的角度詳加體認；尤其是主持三峽清水祖師廟三十多年來的重建工作，對台灣民間藝術更有了進一步的深切了解，從而對環境周遭的一切，產生了微妙深刻的感情。村人們樂天知命，溫良純樸，勤儉克苦的天性，予人非常親切的形象，一直推動著我揮舞彩筆，把自己衷心喜愛的一切事務，展現在畫布上。也可說是他們真誠熱情的生活，豐富了我的繪畫內涵；而我的繪畫目標與理想，亦就是畫出自己所感動所鐘愛的題材，自然而然地形成自我獨特的創作方向與風格。</p>
+      <p>可能是受先兄生前一直期望我能在政治方面有所發揮的意念所影響，因此，雖然以繪畫為我一生職志，但也一直熱心參與地方自治，美術運動，美術教育等工作。個中艱辛與困擾，固然所在多有，但總算也盡了最大心力，差堪告慰先兄在天之靈，而俯仰無愧了。</p>
+      <p>藝術無涯，吾生有涯，欣然回首，一甲子之追尋探索，尚未能達到真善美之境界。」</p>
+      <p>1982年10月</p>
+    </div>
+    <div class="paragraph" v-show="language === 'en-US'">
+      <h1>Personal Statement of the Artist</h1>
+      <h2>60 Years of pursuit in truth, benevolence and beauty</h2>
+      <p>"I was born in a family of strong artistic atmosphere. Under the influence of the environment, I have grown great interests in art and music since very young; I am particularly indulged in painting. In 1918, I was admitted to Taiwan Governor-General's National Language School (namely the former Provincial Taipei Normal College today) during Japanese Ruling Period and started accessing formal art education. I then become more fascinated by painting and was inspired to devote my career in painting. I planned to go to the Tokyo Arts School (namely the former Tokyo University of the Arts today) in Japan for advanced study after graduating from the National Normal University.  Unfortunately my father disagreed and I could not go to Japan. Against my will, I returned home to teach after graduating from the National Normal University in 1922 and got married under the command of my father in the following year.</p>
+      <p>My father’s intention was to reckon my hope to study in Japan. However as time passed by, my passion for pursuit of arts did not diminish but motivated me to look forward to this inspiration of realizing my dreams. During my spare time, I focused on the study of arts and painting even more and retuned to my school during summer vacation, participating in the “Summer Art Seminar” formed by Mr. Ishikawa Kin’ichiro. My works painted during the spare time from work have been selected into the first and second Taiwan Exhibition in a row. My family no longer insisted me in developing in politics after understanding my talents and inspiration.</p>
+      <p>My father passed away in 1924 after the following few years, my efforts received the confidence from my older brothers and others. Finally my dreams were realized in 1928 and I went to Japan at the age of 27, in order to advance more in arts. Upon initial arrival to Tokyo, I worked day and night to strengthen my skills in sketches. After four months and in March of the following year, I was fortunate to pass the admission examination of Tokyo Arts School and I majored in western painting department. With the encouragement of my older brother, I was able to committee in painting without any concerns or holding back.</p>
+      <p>Unfortunately good times never last forever and my older brother died of illness when I was about to finish the first semester of my second year. The burden of the family suddenly fell on my and I had no choice but took a semester off to stay in Taiwan temporarily. When my older brother passed away, my family suffered a major blow so my mother and relatives all convinced me to give up learning painting in Japan but accept the local offer for public service. Finally impelled by ideas, I have been able to implement my initial intent and after taking care of the family affairs, I was determined to pack up and went to Japan, in order to continue my unfinished academic work. Perhaps due to my nature, in spite of the prevalence of fauvism and other painting trends sweeping Japanese painting industry and causing great impact during my study in Japan, I have never changed my ideas but insisted in my confidence and perseverance. I was determined to recognize my favorite realistic creation style. In the 50s, when the domestic abstract painting trend emerged with extraordinary excitement, many artists went along with the trend. However none of these could shatter my credo and I still strode forward in this straight and in-depth path to realism, neglecting any criticism, appraises and judgment from the external environment.</p>
+      <p>I spent my childhood living in the plain rural area and so I have developed a profound and incomparable relation with my hometown. After I grew up and involved in politics for many years, I was able to experience the traditional living culture through a completely different perspective. Particularly the hosting of reconstruction for Sanxia Qingshui Zushi Temple over the last 30 some years, I have developed further profound understanding of the civil arts in Taiwan, thereby  generating a subtle and profound relation with everything in the peripheral environment. The village people are content with life, mild and plain; whose diligent nature leaves people with very friendly image. They have been pushing me to paint and exhibit everything I love on the canvas. One can also say that their honest and passionate life enrich the content of my painting while my painting objectives and ideas are aimed to paint the topics which I love and motivates me, which naturally forms the direction and style of my exceptional creation.</p>
+      <p>Perhaps due to the influence of my older brother who expected me to develop in politics before he passed away, I have also been participating in local self-governance, art movement and art education with enthusiasm in spite the painting has been the career of my life.  I have done my best efforts in spite of the many difficulties and troubles. I can now face the spirit of my deceased older brother without any regret and shame.</p>
+      <p>There is no boundary or art but there is limit to my life. When I look back, I have been pursuing and exploring over the last 60 years but still have not reached the state of truth, benevolence and beauty.</p>
+      <p>October, 1982</p>
+    </div>
+    <div class="paragraph" v-show="language === 'ja'">
+      <h1>画家の言葉</h1>
+      <h2>六十年の真善美の追求</h2>
+      <p>「私は芸術的雰囲気の豊かな家庭で育ち、その環境から薫陶を受けたため、幼いころから美術や音楽に非常に興味があり、特に絵画に心酔していた。1918年に日本統治期の台湾総督府国語学校（現在の省立台北師範専門学校の前身）に合格し、正式な美術教育に触れ始めた後には、さらに絵に夢中になり、絵画で身を立てようと、師範学校卒業後は日本に行って東京美術学校（現在の日本の国立東京芸術大学の前身）に進学しようと計画した。だが残念にも父がそれを許さず、実行は出来なかった。仕方なく、1922年に師範学校を卒業後、故郷に帰って教員となり、父の命に従ってこの年結婚した。</p>
+      <p>父の願いは、これで私が日本に渡って勉強したいという考えを無くすことだった。しかし、月日の流れは私の芸術追求の情熱を静まらせるどころか、かえってさらなる切望に駆り立て、早くこの心からの理想が実現することを願うようになった。そのため、授業の余暇にはさらに画法の研究に専念し、また夏休みを利用して母校に戻り、石川欽一郎先生の組織された「夏期美術講習会」に参加した。仕事の合間に描いた作品は、台展の第一回、第二回に連続入選し、家族もそのため私の素質と志向を理解してくれ、私を頑なに政治方面の道に進ませようとすることは無くなった。</p>
+      <p>1924年に父が死去し、その数年後私の努力の成果は兄らの信頼を得て、ついに1928年宿願叶って、27歳の年に決然として日本での修学へと赴いた。それによって、芸術のさらなる高みを目指し、大志をのばしていくことを期したのである。まずは東京に着いて、日夜デッサンの鍛錬強化に努め、過ぎること四ヵ月、翌年三月に幸運にも東京美術学校入学試験に合格し、西洋画科に学ぶこととなった。兄の励ましがあっために何ら後顧の憂いなく、絵のことに全力を傾けた。</p>
+      <p>ところが良いことはいつまでも続かない。私の二年生の第一学期が終わろうとするときに、兄が突然病で世を去ったのである。家の重責が直ちに私の両肩にのしかかって来た。やむをえず一学期を休学し、台湾にしばらく留まった。兄の死が家族中に与えた打撃は大きく、母や親友たちが口々に私に向かって日本に戻って絵を学ぶことを諦め、地元の手厚い公職に就くよう勧めて来た。しかし、理想への衝動によって私は断固として初心を貫徹し、家のことを適切に処理し終わると、決然として再度日本に赴き、未完の学業を再開した。あるいは天性がそうさせるのかもしれないが、日本での修学時期、野獣派などの絵画思潮が日本の画壇に流行し、大きな衝撃をもたらした。だが、終始自身の理想は揺らぐことなく、依然として一貫した自信と思い入れを保ち続け、自身の好む写実の創作スタイルを堅く肯定し続けたのである。五十年代、国内では抽象絵画の画風が急速に広がり、盛り上がること甚だしく、しばらくは雷同の声が四方から起こったが、これら一切も私の信念を動揺させることなく、相変わらず写実へと真っすぐに深まっていくこの道を昂然と前進し、外界がどのような批評や褒貶をしようが気にしなかった。</p>
+      <p>幼少のころずっと純朴な郷里の野で生活したため、故郷に対しては格別な深い想いがある。壮年から政治に長く関わった後、伝統的な生活文化について、新たに全く別の角度から体験を深めることになった。とくに三峡清水祖師廟の三十数年来の再建工事によって、台湾民間藝術に対して更に深い理解を得た。それにより、周辺環境のあらゆることに、奥深い感情が生まれたのである。村人達の持つ、天命に生き、善良純朴、勤労刻苦の天性は、非常に親しみのあるイメージを与え、常に私に絵筆を揮わせ、心から愛する全ての事物をキャンバスの上に出現させた。彼らの誠実で暖かい生活は、私の絵画の内面を豊かにしたとも言えるだろう。そして、私の絵画における目標と理想とは、すなわち自身が感動し愛した題材を描き出すことであり、それは自ずと私独自の創作の方向と風格を形成した。</p>
+      <p>兄は生前に私が政治方面に力を発揮して欲しいとずっと望んでおり、恐らくその願いの影響を受けて、むろん絵画は私の一生の職であるものの、地方自治、美術運動、美術教育といった仕事にも常に熱心に参加してきた。その中には辛いことや苦悩も、もちろんたくさんあったが、いずれにせよ心身を最大限に尽くしてきた。いささかでも天にいる亡き兄の霊に報告することが出来るかと思うと、悔いるところは少しも無い。</p>
+      <p>芸術には果てが無く、我が一生には終わりがある。振り返ると、六十年の追求と探索は、まだ真善美の境地には達していないようである。」</p>
+      <p>1982年10月</p>
+    </div>
   </div>
 </template>
 
 <script>
-  // export default {
-  //
-  // }
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['language'])
+    }
+  }
 </script>
