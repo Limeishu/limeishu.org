31c62f51aceacf75d7ba6c874d9071a1857483ce
Aries Cs <aries0d0f@gmail.com>
2018-02-10 23:26:18 +0800

    Add creation view components.

---
+++ src/components/layout/CreationView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="spotlight inner">
+    <div class="paragraph" v-if="creation.meta">
+      <h1>{{ creation.name }}</h1>
+      <img :src="`https://image.limeishu.org.tw/images/museum/${creation.image}.jpg`" alt="">
+      <ul>
+        <li>{{ creation.category }}</li>
+        <li>{{ `${creation.meta.type} ${creation.meta['size-cm']} (${creation.meta.size})` }}</li>
+        <li>李梅樹, {{ creation.date }}</li>
+      </ul>
+      <p v-for="(content, i) in creation.content[language]" :key="i">{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapActions, mapGetters } from 'vuex'
+  export default {
+    data () {
+      return {
+        creation: {}
+      }
+    },
+    computed: {
+      ...mapGetters(['language'])
+    },
+    mounted () {
+      this.getCreation()
+    },
+    methods: {
+      ...mapActions(['getCreationByID']),
+      async getCreation () {
+        try {
+          const res = await this.getCreationByID(this.$route.params.id)
+          this.creation = res.data
+        } catch (err) {
+          console.log(new Error(err))
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
