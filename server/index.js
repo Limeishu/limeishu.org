64e0b9987b1ed371cdf715337e49a66a01a9a6da
Aries Cs <aries0d0f@gmail.com>
2018-02-17 07:36:24 +0800

    Fix error page redirect.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const app         = new (require('koa'))()
+const send        = require('koa-send')
 const path        = require('path')
-const hAF         = require('koa2-connect-history-api-fallback')
 const config      = require('./config.json')
 
-app.use(hAF())
 app.use(require('koa-static')(path.join(__dirname, '/../dist/')))
+app.use(async (ctx, next) => {
+  await send(ctx, (path.join(__dirname, '/../dist/index.html')))
+})
 app.listen(config.port)
 console.log(`Start server on port ${config.port}.`)
