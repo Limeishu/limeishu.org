f9f00a73465fe44e931efb4a0ba77ceff3f6018f
Aries Cs <aries0d0f@gmail.com>
2018-02-16 16:03:43 +0800

    Optimize slider navigation font load.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,14 @@
       <h1 v-show="language === 'zh-TW'">最新消息</h1>
       <h1 v-show="language === 'en-US'">What's New</h1>
       <h1 v-show="language === 'ja'">最新ニュース</h1>
-      <carousel :loop="true" :autoplayTimeout="3000" :perPage="device.isMobile ? 1 : 3" :autoplayHoverPause="true" :navigationEnabled="true" :navigationPrevLabel="'&#xf104;'" :navigationNextLabel="'&#xf105;'"
-        class="slider-container">
+      <carousel class="slider-container"
+      :loop="true"
+      :autoplayTimeout="3000"
+      :perPage="device.isMobile ? 1 : 3"
+      :autoplayHoverPause="true"
+      :navigationEnabled="true"
+      navigationNextLabel="<svg aria-hidden='true' data-fa-processed='' data-prefix='fas' data-icon='angle-right' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512' class='svg-inline--fa fa-angle-right fa-w-8'><path fill='currentColor' d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'></path></svg>"
+      navigationPrevLabel="<svg aria-hidden='true' data-fa-processed='' data-prefix='fas' data-icon='angle-left' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512' class='svg-inline--fa fa-angle-left fa-w-8'><path fill='currentColor' d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z' class=''></path></svg>">
         <slide v-for="(news, i) in news.data" :key="i">
           <div class="slider">
             <img :src="news.meta.image">
