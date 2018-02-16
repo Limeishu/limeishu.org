752d95a0ab48c2abee230f939b6c9323e41dc9e8
Aries Cs <aries0d0f@gmail.com>
2018-02-16 17:54:15 +0800

    Use koa-prerender to advancement SEO.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const app         = new (require('koa'))()
 const path        = require('path')
 const hAF         = require('koa2-connect-history-api-fallback')
+const prerender   = require('koa-prerender')
 const config      = require('./config.json')
 
 app.use(hAF())
 app.use(require('koa-static')(path.join(__dirname, '/../dist/')))
+app.use(prerender())
 app.listen(config.port)
 console.log(`Start server on port ${config.port}.`)
