2ff4ca191717062b5fe712b7196519aabcfca63b
Aries Cs <aries0d0f@gmail.com>
2018-07-01 07:40:25 +0800

    Improve menu UI/UX for mobile devices.

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -8,12 +8,12 @@
       <img src="../../assets/images/icons/logo.svg" alt="">
     </div>
     <ul>
-      <li v-for="(item, i) in menu" :key="i">
+      <li v-for="(item, i) in menu" :key="i" :class="{'active': selected[0] === i}" @click="selected= [i, null]">
         <router-link :to="item.children ? '' : item.path" v-if="item.path">
-          <span class="uppercase">{{ item.meta.label[language] || item.name }}</span>
+          <span class="uppercase">{{ item.meta.label[language] || item.name }}</span><span class="icon"><font-awesome-icon v-if="device.isMobile && item.children" icon="angle-right" /></span>
         </router-link>
         <ul v-if="item.children">
-          <li v-for="(item, j) in item.children" :key="j">
+          <li v-for="(item, j) in item.children" :key="j" :class="{'active': selected[0] === i && selected[1] === j}">
             <router-link :to="item.path" v-if="item.path">
               <span class="uppercase">{{ item.meta.label[language] || item.name }}</span>
             </router-link>
@@ -37,7 +37,7 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
   data () {
     return {
-      selected: [0, 0]
+      selected: [...Array(2)]
     }
   },
   computed: {
@@ -62,7 +62,8 @@ export default {
     }
   },
   watch: {
-    $route () {
+    $route (route) {
+      if (route.path === '/') this.selected = [...Array(2)]
       this.pageDetect()
     }
   }
