c0e0e2ee6e02b5e3dbb757699e096a80235f9a5b
Aries Cs <aries0d0f@gmail.com>
2018-07-01 10:58:13 +0800

    UX improve: larger checkable range.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,13 +14,13 @@
       navigationNextLabel="<svg aria-hidden='true' data-fa-processed='' data-prefix='fas' data-icon='angle-right' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512' class='svg-inline--fa fa-angle-right fa-w-8'><path fill='currentColor' d='M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z'></path></svg>"
       navigationPrevLabel="<svg aria-hidden='true' data-fa-processed='' data-prefix='fas' data-icon='angle-left' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 256 512' class='svg-inline--fa fa-angle-left fa-w-8'><path fill='currentColor' d='M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z' class=''></path></svg>">
         <slide v-for="(news, i) in news.data" :key="i" v-if="i <= 6">
-          <div class="slider">
+          <div class="slider" @click="$router.push(`/culture/news/${news._id}`)">
             <img :src="news.meta.image">
             <div class="content">
               <h2>{{ news.title }}</h2>
               <hr>
               <p>{{ `${news.content.substring(0, 100)}...` }}</p>
-              <router-link :to="`/culture/news/${news._id}/index.html`" class="button">
+              <router-link :to="`/culture/news/${news._id}`" class="button">
                 <span>了解更多</span>
                 <font-awesome-icon icon="angle-right" />
               </router-link>
