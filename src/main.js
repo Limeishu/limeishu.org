63dc08634237d6e4cbc7f54dfea3a68aad8ea067
Aries Cs <aries0d0f@gmail.com>
2018-08-11 01:28:57 +0800

    Package: update fontawesome

--- src/main.js
+++ src/main.js
@@ -7,48 +7,18 @@ import VueCookie    from 'vue-cookie'
 import VueCarousel  from 'vue-carousel'
 import VueAnalytics from 'vue-ua'
 
+// Fontawesome
+// the component
+import { FontAwesomeIcon }  from '@fortawesome/vue-fontawesome'
+// the library
+import { library }          from '@fortawesome/fontawesome-svg-core'
+// add more icon categories as you want them, even works with pro packs
+import { fab }              from '@fortawesome/free-brands-svg-icons'
+import { fas }              from '@fortawesome/free-solid-svg-icons'
+
 import App          from './App'
 import router       from './router'
 import store        from './store'
-
-import FontAwesomeIcon                from '@fortawesome/vue-fontawesome'
-import FontAwesome                    from '@fortawesome/fontawesome'
-import {
-  faFacebookF, faTwitter, faGithubAlt, faYoutube, faGithub
-}                                     from '@fortawesome/fontawesome-free-brands'
-import {
-         faAngleLeft,
-         faAngleRight,
-         faCircle,
-         faCircleNotch,
-         faHome,
-         faCaretDown,
-         faHeart,
-         faCartPlus,
-         faPlus,
-         faMinus,
-         faCreditCard,
-         faTruck,
-         faEnvelope,
-         faKey,
-         faTimes,
-         faCheckCircle,
-         faExclamationCircle,
-         faCheck,
-         faGift,
-         faShareAlt,
-         faFax,
-         faPhone,
-         faTrain,
-         faBus,
-         faSubway,
-         faCar,
-         faBars,
-         faGlobe
-       }            from '@fortawesome/fontawesome-free-solid'
-
-FontAwesome.library.add(faFacebookF, faFax, faPhone, faTrain, faGlobe, faBus, faSubway, faCar, faFacebookF, faShareAlt, faYoutube, faGithub, faBars, faAngleLeft, faGithubAlt, faAngleRight, faCircle, faCircleNotch, faHome, faCaretDown, faHeart, faCartPlus, faPlus, faMinus, faCreditCard, faTruck, faEnvelope, faKey, faTimes, faCheckCircle, faExclamationCircle, faCheck, faGift, faShareAlt, faTwitter)
-Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 
 Vue.router = router
 Vue.use(VueAxios, axios)
@@ -58,19 +28,18 @@ Vue.use(VueCarousel)
 Vue.use(VueCookie)
 
 Vue.use(VueAnalytics, {
-  // [Required] The name of your app as specified in Google Analytics.
   appName: 'limeishu.org',
-  // [Required] The version of your app.
   appVersion: 'v2.0.0',
-  // [Required] Your Google Analytics tracking ID.
   trackingId: 'UA-97328395-1',
-  // If you're using vue-router, pass the router instance here.
   vueRouter: router
 })
 
 require('./assets/images/favicon.ico')
 
-// require('./assets/images/favicon.ico')
+library.add(fab)
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 // Enable devtools
 Vue.config.devtools = true
 
@@ -83,4 +52,4 @@ const app = new Vue({
   ...App
 }).$mount('#app')
 
-export { app, router, store, FontAwesome }
+export { app }
