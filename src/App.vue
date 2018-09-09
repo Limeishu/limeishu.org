b7d1b1636c747eddae0f7284eff30d709fce4560
Aries Cs <aries0d0f@gmail.com>
2018-09-09 23:50:42 +0800

    Fix: wrong meta og:url content

--- src/App.vue
+++ src/App.vue
@@ -89,7 +89,9 @@
         }
       },
       getTitle (route) {
-        document.querySelectorAll('head>meta')[5].content = document.title = `${route.meta.label ? route.meta.label[this.language] + ' - ' : ''}${this.title}`
+        const head = document.querySelectorAll('head>meta')
+        head[5].content = document.title = `${route.meta.label ? route.meta.label[this.language] + ' - ' : ''}${this.title}`
+        head[7].content = `https://limeishu.org.tw${document.location.pathname}}`
       },
       eggHandler (event) {
         this.keyListCode += event.keyCode
